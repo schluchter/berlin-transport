d7ab6490acca6382353c32bdd8a5313e555eb144
Thomas Schluchter <tschluchter@salesforce.com>
2014-09-30 01:23:04 +0200

    Adding bridging header

---
+++ berlin-transport/berlin-transport/BerlinTransportBridgingHeader.h
@@ -0,0 +1,16 @@
+//
+//  BerlinTransportBridgingHeader.h
+//  berlin-transport
+//
+//  Created by Thomas Schluchter on 9/30/14.
+//  Copyright (c) 2014 Thomas Schluchter. All rights reserved.
+//
+
+#ifndef berlin_transport_BerlinTransportBridgingHeader_h
+#define berlin_transport_BerlinTransportBridgingHeader_h
+
+import <Ono/Ono.h>
+import <AFNetworking/AFNetworking.h>
+
+
+#endif
