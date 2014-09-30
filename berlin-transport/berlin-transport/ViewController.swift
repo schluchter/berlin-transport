dd9b9af9600cb6438a603adfab532f528d439aef
Thomas Schluchter <tschluchter@salesforce.com>
2014-09-30 16:09:54 +0200

    Adding CoreLocation and MapKit frameworks

--- berlin-transport/berlin-transport/ViewController.swift
+++ berlin-transport/berlin-transport/ViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let mapFrame = self.view.bounds
+        let mapView = MKMapView(frame: mapFrame)
+        self.view.addSubview(mapView)
+        
+        let locMgr = CLLocationManager()
+        if !CLLocationManager.locationServicesEnabled() {
+            println("No location services enabled")
+        } else {
+            println(CLLocationManager.authorizationStatus())
+            locMgr.startUpdatingLocation()
+            let coord = CLLocationCoordinate2D(latitude: 52.518338, longitude: 13.401026)
+            mapView.setRegion(MKCoordinateRegion(center: coord, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        println(__FUNCTION__)
+        mapView.setCenterCoordinate(mapView.userLocation.location.coordinate, animated: true)
     }
 
 
