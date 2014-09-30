c8b114333e6d7f4c94112a4501045558d0c113ad
Thomas Schluchter <tschluchter@salesforce.com>
2014-09-30 18:41:25 +0200

    Adding Example API reponses

---
+++ berlin-transport/berlin-transport/BTRequestParser.swift
@@ -0,0 +1,11 @@
+//
+//  BTRequestParser.swift
+//  berlin-transport
+//
+//  Created by Thomas Schluchter on 9/30/14.
+//  Copyright (c) 2014 Thomas Schluchter. All rights reserved.
+//
+
+import Foundation
+
+
