84d23f522d3dec8ddec922217ab90e17e7d1967f
Thomas Schluchter <tschluchter@salesforce.com>
2014-10-01 15:54:14 +0200

    Getting XML Parsing infrastructure to work

--- berlin-transport/berlin-transport/BTRequestParser.swift
+++ berlin-transport/berlin-transport/BTRequestParser.swift
@@ -8,4 +8,18 @@
 
 import Foundation
 
-
+class BTRequestParser {
+    init() {
+        let xmlPath = NSBundle.mainBundle().URLForResource("VBB-STD-LUAX-CONRes", withExtension: "xml")
+        let xmlData = NSData(contentsOfURL: xmlPath!, options: nil, error: nil)
+        
+        let err = NSError(domain: "XML couldn't be parsed", code: 1, userInfo: nil)
+        
+        if (xmlData != nil) {
+            let hafasRes = ONOXMLDocument(data: xmlData, error: nil)
+            hafasRes.rootElement.enumerateElementsWithXPath("//Connection", usingBlock: { (element, idx, done) -> Void in
+                println("For tag \(element.tag)[\(idx)], the number of attributes is \(element.attributes.count)")
+            })
+        }
+    }
+}
