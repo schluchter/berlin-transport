41cc3833d436231dd9c9520a727b1230128c860a
Thomas Schluchter <tschluchter@salesforce.com>
2014-10-02 17:21:58 +0200

    Bridging header for test target

---
+++ berlin-transport/BerlinTransportTestsBridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  BerlinTransportTestsBridgingHeader.h
+//  berlin-transport
+//
+//  Created by Thomas Schluchter on 10/2/14.
+//  Copyright (c) 2014 Thomas Schluchter. All rights reserved.
+//
+
+#ifndef berlin_transport_BerlinTransportTestsBridgingHeader_h
+#define berlin_transport_BerlinTransportTestsBridgingHeader_h
+
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#endif
