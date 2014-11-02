eb24f79e9932cdaf7b09fbb36e595ccb7fdcf3cd
Thomas Schluchter <tschluchter@salesforce.com>
2014-11-02 09:17:57 -0800

    Adding color constants

--- berlin-transport/berlin-transport/BTConstants.swift
+++ berlin-transport/berlin-transport/BTConstants.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 public let kBTCoordinateDegreeDivisor: Double = 1000000.0
 public let kBTHafasServerURL: String = "http://demo.hafas.de/bin/pub/vbb/extxml.exe"
@@ -18,3 +19,6 @@ public let kBTSecondsPerMinute = 60
 
 public let kBTSecondsPerHour = kBTMinutesPerHour * kBTSecondsPerMinute
 public let kBTSecondsPerDay = kBTHoursPerDay * kBTMinutesPerHour * kBTSecondsPerMinute
+
+// Color constants
+public let kBTPrimaryBgColor = UIColor(hue: 190, saturation: 30, brightness: 22, alpha: 100)
