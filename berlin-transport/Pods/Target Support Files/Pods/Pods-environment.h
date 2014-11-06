6862780b45002df8449eeb4f937db25853c92c22
Thomas Schluchter <tschluchter@salesforce.com>
2014-11-05 18:34:53 -0800

    Adding AFOnoResponseSerializer via CocoaPods

--- berlin-transport/Pods/Target Support Files/Pods/Pods-environment.h
+++ berlin-transport/Pods/Target Support Files/Pods/Pods-environment.h
@@ -48,6 +48,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 1
 
+// AFOnoResponseSerializer
+#define COCOAPODS_POD_AVAILABLE_AFOnoResponseSerializer
+#define COCOAPODS_VERSION_MAJOR_AFOnoResponseSerializer 1
+#define COCOAPODS_VERSION_MINOR_AFOnoResponseSerializer 0
+#define COCOAPODS_VERSION_PATCH_AFOnoResponseSerializer 0
+
 // Ono
 #define COCOAPODS_POD_AVAILABLE_Ono
 #define COCOAPODS_VERSION_MAJOR_Ono 1
