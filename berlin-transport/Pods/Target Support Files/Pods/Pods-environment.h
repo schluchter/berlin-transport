afdea674affc189863c38480f5f7caec9ce1ca1a
Thomas Schluchter <tschluchter@salesforce.com>
2014-11-12 22:31:17 -0800

    Installing Realm.io

--- berlin-transport/Pods/Target Support Files/Pods/Pods-environment.h
+++ berlin-transport/Pods/Target Support Files/Pods/Pods-environment.h
@@ -66,3 +66,15 @@
 #define COCOAPODS_VERSION_MINOR_Ono 1
 #define COCOAPODS_VERSION_PATCH_Ono 3
 
+// Realm
+#define COCOAPODS_POD_AVAILABLE_Realm
+#define COCOAPODS_VERSION_MAJOR_Realm 0
+#define COCOAPODS_VERSION_MINOR_Realm 87
+#define COCOAPODS_VERSION_PATCH_Realm 4
+
+// Realm/Headers
+#define COCOAPODS_POD_AVAILABLE_Realm_Headers
+#define COCOAPODS_VERSION_MAJOR_Realm_Headers 0
+#define COCOAPODS_VERSION_MINOR_Realm_Headers 87
+#define COCOAPODS_VERSION_PATCH_Realm_Headers 4
+
